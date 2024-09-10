75a7c1fa87c68f3e0ef40ff45aad15221aeaca12
DammyFaffy <info4dammyfaffy2022@gmail.com>
2024-09-10 21:05:05 +0100

    Update files with latest changes

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,6 @@
 import {Link, NavLink} from "react-router-dom"
 import Navbar from "./Navbar"
-import { useState } from "react"
+import { useContext, useState } from "react"
 
 // Icons
 import { MdClose, MdMenu } from "react-icons/md"
@@ -10,6 +10,7 @@ import { FaOpencart } from "react-icons/fa"
 import logo from "../assets/logo.svg"
 import logout from "../assets/logout.svg"
 import user from "../assets/user.svg"
+import { ShopContext } from "../Context/ShopContext"
 
 
 
@@ -17,6 +18,7 @@ const Header = () => {
 
     const [menuOpened, setmenuOpened] = useState(false)
     const toogleMenu = () => setmenuOpened(!menuOpened)
+    const {getTotalCartItems} = useContext(ShopContext)
 
 
   return (
@@ -44,7 +46,7 @@ const Header = () => {
                 <MdClose className="md:hidden cursor-pointer hover:text-secondary mr-2 p-1 ring-1 ring-slate-900/30 h-8 w-8 rounded-full" onClick={toogleMenu}/>)}
 
                 <div className="flexBetween sm:gap-x-6">
-                    <NavLink to={"cart-page"} className={"flex"}><FaOpencart className="p-1 h-8 w-8 ring-slate-900/30 ring-1 rounded-full"/><span className="relative flexCenter w-5 h-5 rounded-full bg-secondary text-white medium-14 -top-2">0</span></NavLink>
+                    <NavLink to={"cart-page"} className={"flex"}><FaOpencart className="p-1 h-8 w-8 ring-slate-900/30 ring-1 rounded-full"/><span className="relative flexCenter w-5 h-5 rounded-full bg-secondary text-white medium-14 -top-2">{getTotalCartItems()}</span></NavLink>
                     {/* <NavLink to={'logout'} className={"btn_secondary_rounded flexCenter gap-x-2 medium-16"}><img src={logout} alt="logout" height={19} width={19} />Logout</NavLink> */}
                     <NavLink to={"/login"} className={"btn_secondary_rounded flexCenter gap-x-2 medium-16"}><img src={user} alt="userIcon" height={19} width={19} />Login</NavLink>
                 </div>
---
+++ src/components/ProductDescription.jsx
@@ -0,0 +1,19 @@
+
+
+const ProductDescription = () => {
+  return (
+    <div className="mt-20">
+        <div className="flex gap-3 mb-4">
+            <button className="btn_dark_rounded !rounded-none !text-xs !py-[6px] w-36">Description</button>
+            <button className="btn_dark_outline !rounded-none !text-xs !py-[6px] w-36">Care Guide</button>
+            <button className="btn_dark_outline !rounded-none !text-xs !py-[6px] w-36">Size Guide</button>
+        </div>
+        <div className="flex flex-col pb-16">
+            <p className="text-sm">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Totam, optio? Obcaecati, labore delectus. Asperiores dolores maiores ea provident suscipit, obcaecati quis ipsam in fugit impedit neque enim! Adipisci ex tempora exercitationem quos aliquid nam natus iste atque! Ex, ut deserunt eum dicta debitis voluptate alias, quidem numquam totam assumenda animi? Incidunt ex dicta sunt vel?</p>
+            <p className="text-sm">Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum nihil doloribus est et beatae nesciunt ex expedita. Facere dolorem enim cumque. Eveniet odit soluta consequuntur beatae exercitationem doloribus.</p>
+        </div>
+    </div>
+  )
+}
+
+export default ProductDescription
